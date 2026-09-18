a1c7cb29f3b2bf3aa844551398b1d780b0066337
Conor Shaughnessy <G00432599@atu.ie>
2026-09-18 10:50:35 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/CICDorderservice/CiCdOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.CICDorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/CICDorderservice/CiCdOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.CICDorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
